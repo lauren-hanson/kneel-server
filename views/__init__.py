a1212a1436b33e1335fd6b3d10ea7ab6cac0cf8a
Lauren Hanson <lohanson33@gmail.com>
2023-01-06 15:44:33 -0600

    first commit with views

---
+++ views/__init__.py
@@ -0,0 +1,4 @@
+from .metal_requests import get_all_metals
+from .order_requests import get_all_orders
+from .size_requests import get_all_sizes
+from .style_requests import get_all_styles
---
+++ views/order_requests.py
@@ -0,0 +1,12 @@
+ORDERS = [
+    {
+        "id": 1,
+        "metalId": 1,
+        "sizeId": 2,
+        "styleId": 3
+    }
+]
+
+
+def get_all_orders():
+    return ORDERS
