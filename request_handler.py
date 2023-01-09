33be2709aaca012023c99681faa259a10a6b91cd
Lauren Hanson <lohanson33@gmail.com>
2023-01-09 14:20:23 -0600

    import *

--- request_handler.py
+++ request_handler.py
@@ -1,6 +1,6 @@
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from views import get_all_metals, get_single_metal, get_all_orders, get_single_order, create_order, delete_order, update_order, get_all_sizes, get_single_size, get_all_styles, get_single_style
+from views import *
 
 
 class HandleRequests(BaseHTTPRequestHandler):
