fc00cee9942d3a9bb265cf7852ada44a742eaddd
Lauren Hanson <lohanson33@gmail.com>
2023-01-20 11:12:58 -0600

    SQL delete order

--- views/order_requests.py
+++ views/order_requests.py
@@ -77,34 +77,28 @@ def create_order(new_order):
         new_order['id'] = id 
     return new_order
 
+def delete_order(id): 
+    with sqlite3.connect("./kneeldiamonds.sqlite3") as conn:
+        db_cursor = conn.cursor()
+        db_cursor.execute("""
+        DELETE FROM orders
+        WHERE id = ?
+        """, (id, ))
 
-# def create_order(order):
-#     # Get the id value of the last order in the list
-#     max_id = ORDERS[-1]["id"]
-#     # Add 1 to whatever that number is
-#     new_id = max_id + 1
-#     # Add an `id` property to the order dictionary
-#     order["id"] = new_id
-#     # Add the order dictionary to the list
-#     ORDERS.append(order)
-#     # Return the dictionary with `id` property added
-#     return order
+# def delete_order(id):
+#     # Initial -1 value for order index, in case one isn't found
+#     order_index = -1
 
+#     # Iterate the ORDERS list, but use enumerate() so that you
+#     # can access the index value of each item
+#     for index, order in enumerate(ORDERS):
+#         if order["id"] == id:
+#             # Found the order. Store the current index.
+#             order_index = index
 
-def delete_order(id):
-    # Initial -1 value for order index, in case one isn't found
-    order_index = -1
-
-    # Iterate the ORDERS list, but use enumerate() so that you
-    # can access the index value of each item
-    for index, order in enumerate(ORDERS):
-        if order["id"] == id:
-            # Found the order. Store the current index.
-            order_index = index
-
-    # If the order was found, use pop(int) to remove it from list
-    if order_index >= 0:
-        ORDERS.pop(order_index)
+#     # If the order was found, use pop(int) to remove it from list
+#     if order_index >= 0:
+#         ORDERS.pop(order_index)
 
 
 def update_order(id, new_order):
